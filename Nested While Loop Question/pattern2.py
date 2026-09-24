e2d69f51c024326a3075b0a7b0c90e9d51bca735
Ruthyon21 <salmanmohammadsalman53@gmail.com>
2026-09-24 08:47:37 +0530

    New (*) Patters practice

---
+++ Nested While Loop Question/pattern2.py
@@ -0,0 +1,11 @@
+# Rectangle 3 x 6
+
+i = 1
+
+while i <= 3:
+    j = 1
+    while j <= 6:
+        print("*",end="")
+        j += 1
+    print()
+    i += 1
---
+++ Nested While Loop Question/pattern5.py
@@ -0,0 +1,11 @@
+#  Reverse Rectangle
+
+i = 6
+
+while i >= 1:
+    j = 1
+    while j <= i:
+        print("*",end="")
+        j += 1
+    print()
+    i -= 1
